dfead79b4601639c5b9ae59e435741f8ccd4ae48
Pratik <sarangipratikcode@gmail.com>
2025-03-24 22:32:14 +0530

    Add GitHub Actions deployment

--- src/clients/redis/index.ts
+++ src/clients/redis/index.ts
@@ -1,5 +1,3 @@
 import Redis from "ioredis";
 
-export const redisClient = new Redis(
-  "rediss://default:AWN_AAIjcDExOGMwYzAyZjQxMTA0YjMxOTMxNThjNjRhZmFlYjc0ZnAxMA@endless-cheetah-25471.upstash.io:6379"
-);
+export const redisClient = new Redis(process.env.REDIS_URL as string);
