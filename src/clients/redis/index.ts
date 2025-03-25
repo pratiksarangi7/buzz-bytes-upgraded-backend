1b2af2ae2c060c2ee4805c0372db7f74a14eb156
Pratik <sarangipratikcode@gmail.com>
2025-03-25 23:19:38 +0530

    added dotenv.config() to see if it works

--- src/clients/redis/index.ts
+++ src/clients/redis/index.ts
@@ -1,3 +1,6 @@
+import * as dotenv from "dotenv";
 import Redis from "ioredis";
+
+dotenv.config();
 console.log(process.env.REDIS_URL);
 export const redisClient = new Redis(process.env.REDIS_URL as string);
